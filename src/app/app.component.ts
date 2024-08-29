7731a44b305cf6391b629585d083fd4d78323b3f
Hugo Silva <yhugorocha@gmail.com>
2024-08-29 10:35:57 -0300

    feat: add configurações iniciais do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '<router-outlet>'
 })
 export class AppComponent {
-  title = 'angular-portifolio';
+
 }
